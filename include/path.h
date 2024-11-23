0b14007348308a8596f30e96193c161259e2b773
asdfish <lgray3420@gmail.com>
2024-11-23 08:10:54 -0500

    removed path

--- include/path.h
+++
@@ -1,8 +0,0 @@
-#ifndef PATH_H
-#define PATH_H
-
-#include <stddef.h>
-
-extern int path_get_executables(const char*** executables, size_t* executables_length);
-
-#endif
--- src/main.c
+++ src/main.c
@@ -1,8 +1,25 @@
 #include <utils/string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-  size_t counts = string_count("aaaaaa", "aa");
-  printf("%lu\n", counts);
+  char* path = getenv("PATH");
+  if(!path)
+    return -1;
+
+  const char** arr = NULL;
+  size_t arr_len = 0;
+
+  if(string_separate(path,":",&arr,&arr_len) != 0)
+    return -1;
+
+  if(arr) {
+    for(size_t i = 0; i < arr_len; i ++) {
+      printf("%s\n", arr[i]);
+    }
+    free(arr);
+    arr = NULL;
+  }
+
   return 0;
 }
